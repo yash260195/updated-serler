f2679b235cef78450be18d05f50e357d281353a1
Bharati Koli <bharati.koli@gmail.com>
2017-09-18 00:09:19 +1200

    Serler homepage

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,13 +3,25 @@ var router = express.Router();
 
 //to load main page of the server
 router.get('/', function(req, res){    
-    res.send(`
+    var data = req.app.get('appData');
+    var pagePhotos = [];
+    
+    data.speakers.forEach(function(item) {
+        pagePhotos = pagePhotos.concat(item.artwork);
+    });
+
+    res.render('index', {
+        pageTitle: 'Home',
+        artwork: pagePhotos,
+        pageID: 'home'
+    });
+    /* res.send(`
     <link rel="stylesheet" type="text/css" href="/css/style.css" />
     <h1>SERLER Repository under development</h1>
     <img src="/images/logo/serlerlogo.jpg" alt="serlerlogo" style="height: 300px;" />
     <p>Welcome to news section</p>
     <script src="/reload/reload.js"></script>
-    `);
+    `); */
 }); 
 
 module.exports = router;
